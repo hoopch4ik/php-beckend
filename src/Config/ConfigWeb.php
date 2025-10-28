e202a5e35283cfdecb5a56e408522b77c3068ffe
Nikita <nikitahalturinzak@gmail.com>
2025-10-28 23:05:14 +0300

    change ConfigWeb

--- src/Config/ConfigWeb.php
+++ src/Config/ConfigWeb.php
@@ -4,6 +4,21 @@ namespace App\Config;
 
 class ConfigWeb {
     public const IS_OPEN_API = true;
-    public const SITE_NAME = "Пушкари|Pushkary";
-    public const SITE_DESCRIPTION = "Компания предоставляет продукты в банной сфере";
+    public const IS_CACHED = true;
+    public const IS_LOGGING = false;
+
+    public const SITE_NAME = "";
+    public const SITE_DESCRIPTION = "";
+    public const SITE_DOMAIN = "http://backend";
+    public const ORIGINS = [
+        "http://frontend",
+    ];
+
+    public static function botToken() {
+        return $_ENV['BOT_TOKEN'];
+    }
+
+    public static function tgChatId() {
+        return $_ENV['TG_CHAT_ID'];
+    }
 }
